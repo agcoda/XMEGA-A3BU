8c8b0109d792532e8650f61ed7884578d540ec05
agcoda <agcoda@gmail.com>
2023-10-11 18:04:04 -0700

    refactor to fix warnings

--- Assignment1/Xmega-exploration/Xmega-exploration/Debug/src/main.d
+++
@@ -1,3 +0,0 @@
-src/main.d src/main.o: ../src/main.c ../src/config/devreg.h
-
-../src/config/devreg.h:
--- Week3/flashingLED/flashingLED/src/main.c
+++ Week3/flashingLED/flashingLED/src/main.c
@@ -1,71 +1,74 @@
+/*
+ * main.c
+ *
+ * Created: 10/10/2023 8:17:17 PM
+ *  Author: Alex Gauf
+ */ 
 
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
- */
-#include <asf.h>
+/* Assignment
+1a. Create an include file which contains hardware specific information
+	
+1b. Create wrapper functions to encapsulate Register Read/Write operations
+	
+2.Turn ON and OFF the LEDs continuously with the ON and OFF duration
+to be approximately between 1 and 2 seconds.
+3.Use a software loop to create a delay routine to control the ON and OFF
+duration
+4.DO NOT use the delay library functions provided in the AVR libraries.
+	
+*/
+
 
 #include <devreg.h>
 
 //protos
 void WriteReg(volatile unsigned char *, unsigned char );
-volatile unsigned char * ReadReg(volatile unsigned char *);
-void delay();
+volatile unsigned char ReadReg(volatile unsigned char *);
+void Delay(int);
+void HmSDelay(void);
 
 int main (void)
+
 {
 	static unsigned char ledStates[2] = {0xff, 0xfc}; //{all off, all on}
-	unsigned char stateSwitch = 0;
-	/*
-	1a. Create an include file which contains hardware specific information
-	
-	1b. Create wrapper functions to encapsulate Register Read/Write operations
-	
-	2.Turn ON and OFF the LEDs continuously with the ON and OFF duration
-	to be approximately between 1 and 2 seconds.
-	3.Use a software loop to create a delay routine to control the ON and OFF
-	duration
-	4.DO NOT use the delay library functions provided in the AVR libraries.
-	
-	*/
+	unsigned char stateSwitch = 1;
 		
 	//set PORTB DATA direction (DDRB) as output
-	WriteReg(PORTB_DATA,0xff);
-	
-	
-		
+	WriteReg((volatile unsigned char *)PORTB_DATA,0xff);
+			
 	while(1){
-		
-		//Turn LED0 ON and LED 1 OFF by writing 0xFE to output PORTB,
-		//bit 0 set to 0 to turn on LED 0
+		//Alternate writing ..00 and ..11 to the LED register
 		WriteReg((volatile unsigned char *)PORTB_LED_STATE,ledStates[stateSwitch]);
 		
 		//delay 15*100ms should give ~1.5s per on/off cycle
-		for(int i =0; i<15;i++){
-			delay();
-		}
+		Delay(15);
+		
 		stateSwitch = (stateSwitch+1)%2; 
 	}
 }
 
 //write Value to RegAddress 
-void WriteReg(volatile unsigned char * RegAddress, unsigned char Value){
-	*(RegAddress) = Value;
+void WriteReg(volatile unsigned char * p_regAddress, unsigned char Value){
+	*(p_regAddress) = Value;
 }
 
 //read value in Regaddress and return 
-volatile unsigned char * ReadReg(volatile unsigned char * RegAddress){
-	return *(RegAddress);
+volatile unsigned char ReadReg(volatile unsigned char * p_regAddress){
+	return *(p_regAddress);
 }
 
-//Read the state of the LEDs to force a delay
-//Hard coded to delay 100ms
-void delay(){
-		for(int i=0;i<H_MS_DELAY_CT;i++){
-			ReadReg(PORTB_LED_STATE);
-		}
+//delay an integer number of 100ms (ds)
+void Delay(int dS){
+	for(int i =0; i<dS;i++){
+		HmSDelay();
+	}
+}
+
+//Use register read to delay approximately 100ms.
+void HmSDelay(){
+	for(int i=0;i<H_MS_DELAY_CT;i++){
+		ReadReg((volatile unsigned char *)PORTB_LED_STATE);
+	}
 			
 }
+
